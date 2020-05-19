e89c7d1aceaee38f2c72e1492caeb6ec88257ce3
Ajay Kumar <ajaykumar201761@gmail.com>
2020-05-19 20:16:01 +0530

    Chat Screen Completed

--- lib/widgets/favourite_contacts.dart
+++ lib/widgets/favourite_contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterchatuimessenger/models/message_model.dart';
+import 'package:flutterchatuimessenger/screens/chat_screen.dart';
 
 class FavouriteContacts extends StatefulWidget {
   @override
@@ -40,23 +41,34 @@ class _FavouriteContactsState extends State<FavouriteContacts> {
                 scrollDirection: Axis.horizontal,
                 itemCount: favorites.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 35.0,
-                          backgroundImage: AssetImage(favorites[index].imageUrl),
+                  return GestureDetector(
+                    //Navigating to chat Screen OnTap
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => ChatScreen(
+                          user: favorites[index],
                         ),
-                        SizedBox(height: 6.0),
-                        Text(
-                          favorites[index].name,
-                          style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 35.0,
+                            backgroundImage: AssetImage(favorites[index].imageUrl),
+                          ),
+                          SizedBox(height: 6.0),
+                          Text(
+                            favorites[index].name,
+                            style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 }),
